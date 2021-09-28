3521df74d44269b876a4f150e782050b646f6df3
Hüsnü Taş <husnutas@outlook.com>
2021-09-29 00:03:41 +0300

    Initial Commit

---
+++ HW2_HusnuTasUITests/HW2_HusnuTasUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HW2_HusnuTasUITestsLaunchTests.swift
+//  HW2_HusnuTasUITests
+//
+//  Created by Hüsnü Taş on 29.09.2021.
+//
+
+import XCTest
+
+class HW2_HusnuTasUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
